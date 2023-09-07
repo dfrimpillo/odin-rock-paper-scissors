22f8810c5062599e5e238f1992b7e38c35770d5a
dfrimpillo <rimpillodesserie@gmail.com>
2023-09-08 00:11:19 +0800

    added event listener to the buttons and prints result

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+  }
+  
+  function playRound(playerSelection) {
 
-let numberOfRounds = 5;
-
-function game(numberOfRounds) {
-
-    for (let i = 1; i <= numberOfRounds; i++) {
-
-        const playerSelection  = prompt ("Choose: Rock, Paper or Scissors?");
-        console.log(`Player choice: ${playerSelection}`);
-        
-        function getComputerChoice(){
-           const choices = ["Rock", "Paper", "Scissors"];
-           const randomIndex = Math.floor(Math.random() * 3);
-           return choices[randomIndex];
-        }
-        
-        
-        
-        const computerSelection = getComputerChoice();
-        
-        console.log(`Computer choice: ${computerSelection}`);
-        
-        if (playerSelection === null) {
-            console.log("Input error!");
-            break;
-        } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-            console.log("It's a draw");
-        } else if (
-            (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") ||
-            (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") ||
-            (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
-        ) {
-          console.log(`You win! ${playerSelection} beats ${computerSelection}`);  
-        } else {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-        }
-
+    const computerSelection = getComputerChoice();
+    const displayResult = document.getElementById('result');
+  
+    if (playerSelection === computerSelection) {
+        displayResult.textContent = "It's a draw";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        displayResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        displayResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     }
-}
+  }
+  
+  const btn = document.querySelectorAll('#buttons img');
 
-game(numberOfRounds);
-
-alert ("Play another round!");
+btn.forEach(button => {
+  button.addEventListener('click', function() {
+    const playerSelection = button.className;
+    playRound(playerSelection);
+  });
+});
